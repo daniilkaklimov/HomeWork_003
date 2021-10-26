98bd2895a48f9e661d13ab71360cf9148b6e0464
Daniil <yourdallero@gmail.com>
2021-10-26 18:44:39 +0700

    Домашняя работа, урок 4

--- app/src/main/java/ru/klimov/homework3/MainActivity.java
+++ app/src/main/java/ru/klimov/homework3/MainActivity.java
@@ -1,92 +1,67 @@
 package ru.klimov.homework3;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    Button button0;
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
-    Button button5;
-    Button button6;
-    Button button7;
-    Button button8;
-    Button button9;
-    Button button10;
-    Button button11;
-    Button button12;
-    Button button13;
-    Button button14;
-    Button button15;
-    Button button16;
-    Button button17;
-    Button button18;
+
+    private CalculatorModel calculator;
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constraint_layout);
 
-        textView = (TextView) findViewById(R.id.textView);
-        button0 = (Button) findViewById(R.id.button_0);
-        button1 = (Button) findViewById(R.id.button_1);
-        button2 = (Button) findViewById(R.id.button_2);
-        button3 = (Button) findViewById(R.id.button_3);
-        button4 = (Button) findViewById(R.id.button_4);
-        button5 = (Button) findViewById(R.id.button_5);
-        button6 = (Button) findViewById(R.id.button_6);
-        button7 = (Button) findViewById(R.id.button_7);
-        button8 = (Button) findViewById(R.id.button_8);
-        button9 = (Button) findViewById(R.id.button_9);
-        button10 = (Button) findViewById(R.id.button_10);
-        button11 = (Button) findViewById(R.id.button_11);
-        button12 = (Button) findViewById(R.id.button_12);
-        button13 = (Button) findViewById(R.id.button_13);
-        button14 = (Button) findViewById(R.id.button_14);
-        button15 = (Button) findViewById(R.id.button_15);
-        button16 = (Button) findViewById(R.id.button_16);
-        button17 = (Button) findViewById(R.id.button_17);
-        button18 = (Button) findViewById(R.id.button_18);
+        int[] numberIds = new int[] {
+                R.id.button_0,
+                R.id.button_1,
+                R.id.button_2,
+                R.id.button_3,
+                R.id.button_4,
+                R.id.button_5,
+                R.id.button_6,
+                R.id.button_7,
+                R.id.button_8,
+                R.id.button_9
+        };
 
-        View.OnClickListener onClickListener = new View.OnClickListener() {
+        int[] actionIds = new int[] {
+                R.id.button_13, //+
+                R.id.button_12, //-
+                R.id.button_11, //*
+                R.id.button_10, //:
+                R.id.button_14
+        };
+
+        textView = findViewById(R.id.textView);
+
+        calculator = new CalculatorModel();
+
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (view.getId()) {
-                }
-                Button button1 = (Button)view;
-                String buttonText = button1.getText().toString();
-                textView.setText(buttonText);
+                    calculator.NumPressed(view.getId());
+                    textView.setText(calculator.getText());
+            }
+        };
+        View.OnClickListener actionButtonOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculator.ActionPressed(view.getId());
+                textView.setText(calculator.getText());
             }
         };
 
-        button0.setOnClickListener(onClickListener);
-        button1.setOnClickListener(onClickListener);
-        button2.setOnClickListener(onClickListener);
-        button3.setOnClickListener(onClickListener);
-        button4.setOnClickListener(onClickListener);
-        button5.setOnClickListener(onClickListener);
-        button6.setOnClickListener(onClickListener);
-        button7.setOnClickListener(onClickListener);
-        button8.setOnClickListener(onClickListener);
-        button9.setOnClickListener(onClickListener);
-        button10.setOnClickListener(onClickListener);
-        button11.setOnClickListener(onClickListener);
-        button12.setOnClickListener(onClickListener);
-        button13.setOnClickListener(onClickListener);
-        button14.setOnClickListener(onClickListener);
-        button15.setOnClickListener(onClickListener);
-        button16.setOnClickListener(onClickListener);
-        button17.setOnClickListener(onClickListener);
-        button18.setOnClickListener(onClickListener);
+        for (int i = 0; i < numberIds.length; i++) {
+            findViewById(numberIds[i]).setOnClickListener(numberButtonClickListener);
+        }
+        for (int i = 0; i < actionIds.length; i++) {
+            findViewById(actionIds[i]).setOnClickListener(actionButtonOnClickListener);
+        }
     }
 }
-
